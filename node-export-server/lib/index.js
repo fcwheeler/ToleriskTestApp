493e0812a51110663aaa20241ea7583903d65bd7
Ryan <fcwheeler@gmail.com>
2017-08-24 18:49:10 -0400

    remove colors

--- node-export-server/lib/index.js
+++ node-export-server/lib/index.js
@@ -25,7 +25,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 */
 
-require('colors');
+//require('colors');
 
 const logger = require('./logger.js');
 const chart = require('./chart.js');
